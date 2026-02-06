6933f1c3ce0ff01a2484b08e6a5cd013c044f912
samialtash <samialtasgs@gmail.com>
2026-02-06 13:42:46 +0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 // Supabase: anon key'i Supabase Dashboard > Project Settings > API > anon public key'den alıp aşağıya yapıştırın.
 window.SUPABASE_URL = "https://hiyjgxalrecfoydshlgj.supabase.co";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpeWpneGFscmVjZm95ZHNobGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzNzA2MTksImV4cCI6MjA4NTk0NjYxOX0.8B5t-3m_PhrTLXUGVgt_Bg5aohQDaRQ2dXrRPe3I7bI";
