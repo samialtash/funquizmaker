7c15a18d9e7ac4948b229077d0879fa2169334ad
samialtash <samialtasgs@gmail.com>
2026-03-18 00:11:42 +0300

    Add files via upload

--- api/ads.js
+++ api/ads.js
@@ -1,6 +1,6 @@
 // Vercel serverless: /ads.txt - AdSense tam olarak bu formatta bekliyor (satır sonu newline, tek veri satırı)
 // https://support.google.com/adsense/answer/7532444
-const ADS_LINE = 'google.com, pub-1484572525060728, DIRECT, f08c47fec0942fa0';
+const ADS_LINE = "google.com, pub-1484572525060728, DIRECT, f08c47fec0942fa0";
 
 module.exports = function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
